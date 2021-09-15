2f9d179e540933279a5f473888fd07e480970eed
SandyJosipovic <87452807+SandyJosipovic@users.noreply.github.com>
2021-09-16 01:14:22 +1000

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,15 @@
+var prac1 = new Vue({
+  el: '#prac-1',
+  data: {
+    todos: [{
+        text: 'Earn lots of money'
+      },
+      {
+        text: 'buy a house'
+      },
+      {
+        text: 'live'
+      }
+    ]
+  }
+})
